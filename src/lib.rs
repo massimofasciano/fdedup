b95c1ec5d09331d9306fc25565200e66a8eb49bc
Massimo Fasciano <massimo@mafalitic.com>
2023-02-23 11:13:58 -0500

    first test version using Vec<u8> and PathBuf

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::{collections::HashMap};
+
+type HashData = Vec<u8>;
+type PathData = std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct HashedFile {
+    path : PathData,
+    hash : HashData,
+}
+
+impl HashedFile {
+    pub fn new(path : PathData) -> Result<HashedFile, Box<dyn std::error::Error>> {
+        use sha2::{Sha512, Digest};
+        use std::{io, fs};
+
+        let mut hasher = Sha512::new();
+        let mut file = fs::File::open(&path)?;
+        
+        io::copy(&mut file, &mut hasher)?;
+        let hash_bytes = hasher.finalize();
+
+        let hash = hash_bytes.to_vec();
+        let hf = HashedFile { 
+            path, 
+            hash,
+        };
+        Ok(hf)
+    }
+}
+
+#[derive(Debug)]
+pub struct HashedFiles {
+    by_hash : HashMap<HashData,Vec<PathData>>,
+    by_path : HashMap<PathData,HashedFile>,
+}
+
+impl HashedFiles {
+    pub fn new() -> HashedFiles {
+        HashedFiles {
+            by_hash : HashMap::new(),
+            by_path : HashMap::new(),
+        }
+    }
+    pub fn add_file(&mut self, f: HashedFile) {
+        if let Some(v) = self.by_hash.get_mut(&f.hash) {
+            v.push(f.path.clone())
+        } else {
+            self.by_hash.insert(f.hash.clone(), vec!(f.path.clone()));
+        };
+        self.by_path.insert(f.path.clone(), f);
+    }
+    pub fn duplicates(& self) -> impl Iterator<Item=&Vec<PathData>> {
+        self.by_hash.iter().filter_map(|(_,x)| if (*x).len() > 1 {Some(x)} else {None})
+    }
+    pub fn add_path(&mut self, path: PathData) {
+        if let Ok(hf) = HashedFile::new(path) {
+            self.add_file(hf)
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use fdedup::HashedFiles;
+use walkdir::WalkDir;
+
+fn main() {
+    let mut hfs = HashedFiles::new();
+    let walk = WalkDir::new(".").into_iter()
+            .filter_map(|e| e.ok())
+            .map(|e| e.into_path());
+    for entry in walk {
+        hfs.add_path(entry);
+    }
+
+    //println!("{hfs:#?}");
+    println!("{:#?}",hfs.duplicates().collect::<Vec<_>>());
+}
