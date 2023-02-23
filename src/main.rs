e56ba1a2b03f11418fa3f5f7cdc74b00b0ee1c25
Massimo Fasciano <massimo@mafalitic.com>
2023-02-23 11:20:25 -0500

    filter the walk to look at files only

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut hfs = HashedFiles::new();
     let walk = WalkDir::new(".").into_iter()
             .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
             .map(|e| e.into_path());
     for entry in walk {
         hfs.add_path(entry);
