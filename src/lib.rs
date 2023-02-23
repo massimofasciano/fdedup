0e9236f775c5ecb13ef13a9df9a7e7de37cb1c6a
Massimo Fasciano <massimo@mafalitic.com>
2023-02-23 13:14:55 -0500

    add access functions and a new duplicates method

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,41 @@ use std::{collections::HashMap};
 
 type HashData = Vec<u8>;
 type PathData = std::path::PathBuf;
+type MetaData = Option<std::fs::Metadata>;
+type FileSize = u64;
 
 #[derive(Debug)]
 pub struct HashedFile {
     path : PathData,
     hash : HashData,
+    metadata : MetaData,
+    size : FileSize,
 }
 
 impl HashedFile {
-    pub fn new(path : PathData) -> Result<HashedFile, Box<dyn std::error::Error>> {
+    pub fn new(path : PathData, metadata: MetaData) -> Result<HashedFile, Box<dyn std::error::Error>> {
         use sha2::{Sha512, Digest};
         use std::{io, fs};
 
         let mut hasher = Sha512::new();
         let mut file = fs::File::open(&path)?;
-        io::copy(&mut file, &mut hasher)?;
-        Ok(HashedFile{path, hash : hasher.finalize().to_vec()})
+        let size = io::copy(&mut file, &mut hasher)?;
+        Ok(HashedFile{path, hash : hasher.finalize().to_vec(), metadata, size})
+    }
+    pub fn size(&self) -> FileSize {
+        self.size
+    }
+    pub fn metadata(&self) -> &MetaData {
+        &self.metadata
+    }
+    pub fn path(&self) -> &PathData {
+        &self.path
+    }
+    pub fn hash(&self) -> &HashData {
+        &self.hash
+    }
+    pub fn hex_hash(&self) -> String {
+        hex::encode(&self.hash)
     }
 }
 
@@ -34,6 +53,9 @@ impl HashedFiles {
             by_path : HashMap::new(),
         }
     }
+    pub fn get_by_path (&self, path : &PathData) -> Option<&HashedFile> {
+        self.by_path.get(path)
+    }
     pub fn add_file(&mut self, f: HashedFile) {
         if let Some(v) = self.by_hash.get_mut(&f.hash) {
             v.push(f.path.clone())
@@ -42,11 +64,25 @@ impl HashedFiles {
         };
         self.by_path.insert(f.path.clone(), f);
     }
-    pub fn duplicates(& self) -> impl Iterator<Item=&Vec<PathData>> {
-        self.by_hash.iter().filter_map(|(_,x)| if (*x).len() > 1 {Some(x)} else {None})
+    pub fn duplicates_by_path_with_minsize(& self, minsize : FileSize) -> impl Iterator<Item=&Vec<PathData>> {
+        self.by_hash.iter().filter_map(move |(_,x)| {
+            if (*x).len() > 1 && 
+              self.by_path.get(&(*x)[0]).unwrap().size >= minsize {
+                Some(x)
+            } else {
+                None
+            }})
+    }
+    pub fn duplicates(& self) -> impl Iterator<Item=impl Iterator <Item=&HashedFile>> {
+        self.by_hash.iter().filter_map(|(_,x)| {
+            if (*x).len() > 1 {
+                Some((*x).iter().map(|p| self.by_path.get(p).unwrap()))
+            } else {
+                None
+            }})
     }
     pub fn add_path(&mut self, path: PathData) {
-        if let Ok(hf) = HashedFile::new(path) {
+        if let Ok(hf) = HashedFile::new(path,None) {
             self.add_file(hf)
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,17 @@ fn main() {
         hfs.add_path(entry);
     }
 
-    //println!("{hfs:#?}");
-    println!("{:#?}",hfs.duplicates().collect::<Vec<_>>());
+    // println!("{hfs:#?}");
+    // println!("{:#?}",hfs.duplicates_by_path_with_minsize(1024).collect::<Vec<_>>());
+    for group in hfs.duplicates() {
+        let group : Vec<_> = group.collect();
+        if group.len() > 1 {
+            let group_info = group[0];
+            if group_info.size() > 1024 {
+                println!("{} {}",group_info.size(),group_info.hex_hash());
+                println!("{:#?}",group.iter().map(|e| e.path()).collect::<Vec<_>>());
+                println!();
+            }
+        }
+    }
 }
