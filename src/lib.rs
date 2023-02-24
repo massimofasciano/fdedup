4ffdda88879445fa069db1345779a63ddb537fa2
Massimo Fasciano <massimo@mafalitic.com>
2023-02-24 01:57:54 -0500

    move 3 functions from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -140,3 +140,22 @@ impl HashedFiles {
         self.duplicates_with_minsize(0)
     }
 }
+
+pub fn index_dir(hfs : &mut HashedFiles, dir : &str) {
+    let walk = walkdir::WalkDir::new(dir).into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file());
+    for entry in walk {
+        hfs.add_path(entry.path().to_owned(), entry.metadata().unwrap().modified().unwrap());
+    }
+}
+
+pub fn serialize(fname : &str, hfs : HashedFiles) {
+    let bytes = bincode::serialize(&hfs).unwrap();
+    std::fs::write(fname, &bytes[..]).unwrap();
+}
+
+pub fn deserialize(fname : &str) -> HashedFiles {
+    let bytes = std::fs::read(fname).unwrap_or(vec!());
+    bincode::deserialize(&bytes[..]).unwrap_or(HashedFiles::new())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-use fdedup::HashedFiles;
-use walkdir::WalkDir;
-
-fn index_dir(hfs : &mut HashedFiles, dir : &str) {
-    let walk = WalkDir::new(dir).into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| e.file_type().is_file());
-    for entry in walk {
-        hfs.add_path(entry.path().to_owned(), entry.metadata().unwrap().modified().unwrap());
-    }
-}
-
-fn serialize(fname : &str, hfs : HashedFiles) {
-    let bytes = bincode::serialize(&hfs).unwrap();
-    std::fs::write(fname, &bytes[..]).unwrap();
-}
-
-fn deserialize(fname : &str) -> HashedFiles {
-    let bytes = std::fs::read(fname).unwrap_or(vec!());
-    bincode::deserialize(&bytes[..]).unwrap_or(HashedFiles::new())
-}
+use fdedup::{HashedFiles, deserialize, serialize, index_dir};
 
 fn main() {
     let fname = "hfs.bin";
     //let mut hfs = HashedFiles::new();
-    let mut hfs = deserialize(fname);
+    let mut hfs : HashedFiles = deserialize(fname);
     index_dir(&mut hfs, ".");
     for dup in hfs.duplicates() {
         println!("# {} {}",dup.size(), dup.hex_hash());
