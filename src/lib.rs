53f1aba8797b0287018700d7dabd2c93d507eb0c
Massimo Fasciano <massimo@mafalitic.com>
2023-02-23 23:56:52 -0500

    create Duplicates type for results, sort by size

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,31 @@ type MetaData = Option<std::fs::Metadata>;
 type FileSize = u64;
 
 #[derive(Debug)]
+pub struct Duplicates {
+    paths : Vec<PathData>,
+    hex_hash : String,
+    size : FileSize,
+}
+
+impl Duplicates {
+    pub fn new(paths : Vec<PathData>, hex_hash : String, size : FileSize) -> Duplicates {
+        Duplicates { paths, hex_hash, size }
+    }
+    pub fn size(&self) -> FileSize {
+        self.size
+    }
+    pub fn paths(&self) -> &Vec<PathData> {
+        &self.paths
+    }
+    pub fn display_paths(&self) -> impl Iterator<Item=std::path::Display> + '_ {
+        self.paths.iter().map(|p| p.display())
+    }
+    pub fn hex_hash(&self) -> &String {
+        &self.hex_hash
+    }
+}
+
+#[derive(Debug)]
 pub struct HashedFile {
     path : PathData,
     hash : HashData,
@@ -64,22 +89,34 @@ impl HashedFiles {
         };
         self.by_path.insert(f.path.clone(), f);
     }
-    pub fn duplicates_by_path_with_minsize(& self, minsize : FileSize) -> impl Iterator<Item=&Vec<PathData>> {
-        self.by_hash.iter().filter_map(move |(_,x)| {
-            if (*x).len() > 1 && 
-              self.by_path.get(&(*x)[0]).unwrap().size >= minsize {
-                Some(x)
-            } else {
-                None
-            }})
-    }
-    pub fn duplicates(& self) -> impl Iterator<Item=impl Iterator <Item=&HashedFile>> {
+    pub fn duplicates_as_hashed_files(& self) -> impl Iterator<Item=impl Iterator <Item=&HashedFile>> {
         self.by_hash.iter().filter_map(|(_,x)| {
             if (*x).len() > 1 {
                 Some((*x).iter().map(|p| self.by_path.get(p).unwrap()))
             } else {
                 None
             }})
+    }
+    pub fn duplicates_with_minsize(& self, minsize : FileSize) -> Vec<Duplicates> {
+        let mut result = vec!();
+        for group in self.duplicates_as_hashed_files() {
+            let group : Vec<_> = group.collect();
+            if group.len() > 1 {
+                let group_info = group[0];
+                if group_info.size() > minsize {
+                    result.push(Duplicates {
+                        size : group_info.size(),
+                        hex_hash : group_info.hex_hash(),
+                        paths : group.iter().map(|e| e.path().clone()).collect::<Vec<_>>(),
+                    })
+                }
+            }
+        }
+        result.sort_by(|a, b| a.size.cmp(&b.size));
+        result
+    }
+    pub fn duplicates(& self) -> Vec<Duplicates> {
+        self.duplicates_with_minsize(0)
     }
     pub fn add_path(&mut self, path: PathData) {
         if let Ok(hf) = HashedFile::new(path,None) {
--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,11 @@ fn main() {
         hfs.add_path(entry);
     }
 
-    // println!("{hfs:#?}");
-    // println!("{:#?}",hfs.duplicates_by_path_with_minsize(1024).collect::<Vec<_>>());
-    for group in hfs.duplicates() {
-        let group : Vec<_> = group.collect();
-        if group.len() > 1 {
-            let group_info = group[0];
-            if group_info.size() > 1024 {
-                println!("{} {}",group_info.size(),group_info.hex_hash());
-                println!("{:#?}",group.iter().map(|e| e.path()).collect::<Vec<_>>());
-                println!();
-            }
+    for dup in hfs.duplicates() {
+        println!("# {} {}",dup.size(), dup.hex_hash());
+        for p in dup.display_paths() {
+            println!("{}",p)
         }
+        println!();
     }
 }
