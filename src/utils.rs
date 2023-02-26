8984fd961b248afad9774188384ebf478a2aca3c
Massimo Fasciano <massimo@mafalitic.com>
2023-02-26 08:28:06 -0500

    vprintln and vvprintln with cargo features

--- src/utils.rs
+++ src/utils.rs
@@ -4,17 +4,23 @@ pub(crate) type HashData = Vec<u8>;
 pub(crate) type PathData = std::path::PathBuf;
 pub(crate) type FileSize = u64;
 
-// macro_rules! vprintln {
-//     ($($x:tt)*) => { println!($($x)*); }
-// }
-// macro_rules! vvprintln {
-//     ($($x:tt)*) => { println!($($x)*); }
-// }
+#[cfg(feature = "verbose")]
+macro_rules! vprintln {
+    ($($x:tt)*) => { println!($($x)*); }
+}
+#[cfg(not(feature = "verbose"))]
 macro_rules! vprintln {
     ($($x:tt)*) => {  }
 }
+
+#[cfg(feature = "very-verbose")]
+macro_rules! vvprintln {
+    ($($x:tt)*) => { println!($($x)*); }
+}
+#[cfg(not(feature = "very-verbose"))]
 macro_rules! vvprintln {
     ($($x:tt)*) => {  }
 }
-            
+
 pub(crate) use {vprintln,vvprintln}; 
+
