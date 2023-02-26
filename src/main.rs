20dd53d55e550dde7cc9a99cd5f4e89178d544f4
Massimo Fasciano <massimo@mafalitic.com>
2023-02-26 17:48:49 -0500

    add clap command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use fdedup::{Deduplicator,Result};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   folders: Vec<String>,
+   #[arg(short, long, default_value_t = false)]
+   cache: bool,
+   #[arg(long, value_name = "<FILE>")]
+   cache_file: Option<String>,
+   #[arg(short, long, default_value_t = false)]
+   normalize: bool,
+}
 
 fn main() -> Result<()> {
-    let cache = ".fdedup_cache.bin";
+    let args = Args::parse();
+    let cache_default = ".fdedup_cache.bin";
     let mut dedup = Deduplicator::default();
-    dedup.add_dir(".");
-    dedup.normalize_path(true);
-    dedup.read_cache(cache);
+    if args.folders.len() > 0 {
+        for d in args.folders {
+            dedup.add_dir(d.as_ref());
+        }
+    } else {
+        dedup.add_dir(".");
+    }
+    dedup.normalize_path(args.normalize);
+    if let Some(cache_file) = &args.cache_file {
+        dedup.read_cache(cache_file);
+    } else if args.cache {
+        dedup.read_cache(cache_default);
+    }
     dedup.run()?;
-    dedup.write_cache(cache)?;
+    if let Some(cache_file) = &args.cache_file {
+        dedup.write_cache(cache_file)?;
+    } else if args.cache {
+        dedup.write_cache(cache_default)?;
+    }
     Ok(())
 }
