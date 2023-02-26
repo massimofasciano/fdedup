94d591fb15a445b8b80d0745cdde832a39c1483d
Massimo Fasciano <massimo@mafalitic.com>
2023-02-26 16:52:56 -0500

    remove some comments

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,4 @@
-//pub type Result<T> = Result<T, Box<dyn std::error::Error>>;
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-//type Result<T, E = Error> = std::result::Result<T, E>;
 
 pub(crate) type HashData = Vec<u8>;
 pub(crate) type PathData = std::path::PathBuf;
